909c1e47d7449fb9e57db4b83383c06c1141f4bd
Paola Romero <PaolaRomero@paolas-mbp.lan>
2021-04-25 18:55:38 -0500

    Finished Delvierable 2

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library(dplyr)
+MechaCar_mpg <- read.csv('MechaCar_mpg.csv')
+lr_model = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
+summary(lr_model)
+suspension_coil <- read.csv('Suspension_Coil.csv')
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
